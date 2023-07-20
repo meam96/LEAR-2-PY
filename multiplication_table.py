fe150963e9d7db07299a6df7c129ac88c62733e6
Mustafa Mizani <m.mizani95@gmail.com>
2023-07-20 15:21:25 +0330

    new_challenge

---
+++ multiplication_table.py
@@ -0,0 +1,12 @@
+#write a nested for to show a multiplication table
+#1 2 3
+#2 4 6
+#3 6 9
+
+def multiplication_table(start,stop):
+    for x in range(1,4):
+        for y in range(1,4):
+            print(str(x*y), end=" ")
+    
+    print()
+multiplication_table(1,3)
